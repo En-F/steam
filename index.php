7f5e01ff936c6fe8ce3896a85371cfe89ad6ef42
Enrique Franco <enrique.franco@iesdonana.org>
2025-10-16 19:39:51 +0200

    Borrado completo de clientes

--- index.php
+++ index.php
@@ -7,34 +7,20 @@
 </head>
 <body>
     <?php
-    //crea una instancia de la clase PDO el cual establece una conexción con la base de datos
-    $pdo = new PDO('pgsql:host=localhost;dbname=steam;','steam','steam');
+    require 'auxiliar.php';
 
-    //lo guarmadamos como una sentencia donde el valor es una consulta con el query
+    $pdo = conectar();
     $sent = $pdo->query('SELECT * FROM clientes');
-    
-    //forma para mostrar las filas siguientes
-    //True mientras existan filas
-    // foreach( $sent as $fila) {
-    //     echo $fila['dni'];
-    //     echo $fila['nombre'];
-    //     echo $fila['apellidos'];
-    //     echo $fila['codpostal'];
-    //     echo $fila['telefono'];
-    //     echo '<br>';
-    // }
     ?>
-
-<table border="1">
+    <table border="1">
         <thead>
-            <tr>
-                <th>DNI</th>
-                <th>Nombre</th>
-                <th>Apellidos</th>
-                <th>Dirección</th>
-                <th>Código Postal</th>
-                <th>Teléfono</th>
-            </tr>
+            <th>DNI</th>
+            <th>Nombre</th>
+            <th>Apellidos</th>
+            <th>Dirección</th>
+            <th>Código postal</th>
+            <th>Teléfono</th>
+            <th>Acciones</th>
         </thead>
         <tbody>
             <?php foreach ($sent as $fila): ?>
@@ -45,10 +31,15 @@
                     <td><?= $fila['direccion'] ?></td>
                     <td><?= $fila['codpostal'] ?></td>
                     <td><?= $fila['telefono'] ?></td>
+                    <td>
+                        <form action="borrar.php" method="post">
+                            <input type="hidden" name="id" value="<?= $fila['id'] ?>">
+                            <button type="submit">Borrar</button>
+                        </form>
+                    </td>
                 </tr>
             <?php endforeach ?>
         </tbody>
     </table>
-
 </body>
 </html>
--- steam.sql
+++ steam.sql
@@ -3,17 +3,46 @@
 DROP TABLE IF EXISTS clientes CASCADE;
 
 CREATE TABLE clientes (
-    id          BIGSERIAL       PRIMARY KEY,
-    dni         VARCHAR(9)      NOT NULL UNIQUE,
-    nombre      VARCHAR(255)    NOT NULL,
-    apellidos   VARCHAR(255),
-    direccion   VARCHAR(255),
-    codpostal   NUMERIC(5)      CHECK(codpostal >= 0),
-    telefono    VARCHAR(255)
+    id        BIGSERIAL     PRIMARY KEY,
+    dni       VARCHAR(9)    NOT NULL UNIQUE,
+    nombre    VARCHAR(255)  NOT NULL,
+    apellidos VARCHAR(255),
+    direccion VARCHAR(255),
+    codpostal NUMERIC(5)    CHECK (codpostal >= 0),
+    telefono  VARCHAR(255)
 );
 
---Datos de pruebas
+-- Tabla de desarrolladoras
 
-INSERT INTO clientes(dni, nombre, apellidos, direccion, codpostal ,telefono)
-VALUES ('11111111A','Juan','Franco','C/. Su casa',11540,'666555444'),
-       ('22222222A','María','Ulric','C/. Su otra casa',11550,'555444333');
+DROP TABLE IF EXISTS desarrolladoras CASCADE;
+
+CREATE TABLE desarrolladoras (
+    id     BIGSERIAL    PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL
+);
+
+-- Tabla de videojuegos
+
+DROP TABLE IF EXISTS videojuegos CASCADE;
+
+CREATE TABLE videojuegos (
+    id                BIGSERIAL      PRIMARY KEY,
+    nombre            VARCHAR(255)   NOT NULL,
+    salida            TIMESTAMP(0)     NOT NULL,
+    precio            NUMERIC(6,2),
+    desarrolladora_id BIGINT         NOT NULL REFERENCES desarrolladoras (id)
+);
+
+-- Datos de prueba
+
+INSERT INTO clientes (dni, nombre, apellidos, direccion, codpostal, telefono)
+VALUES ('11111111A', 'Juan', 'Martínez', 'C/. Su casa', 11540, '666555444'),
+       ('22222222B', 'María', 'González', 'C/. Su otra casa', 11550, '555444333');
+
+INSERT INTO desarrolladoras (nombre)
+VALUES ('The Game Kitchen'),
+       ('Valve');
+
+INSERT INTO videojuegos (nombre, salida, precio, desarrolladora_id)
+VALUES ('Blasphemus', '2021-04-20 14:12:00', 39.90, 1),
+       ('Half life 3', '2025-11-30 20:00:00', 59.90, 2);
