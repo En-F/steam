20953f032be4e30a5b58e3fd876c9f03ea619091
Enrique Franco <enrique.franco@iesdonana.org>
2025-10-23 18:47:35 +0200

    Modificar de clientes funcionando

--- auxiliar.php
+++ auxiliar.php
@@ -10,6 +10,12 @@ function obtener_post(string $parametro): string|null
     return isset($_POST[$parametro]) ? trim($_POST[$parametro]) : null;
 }
 
+function obtener_get(string $parametro): string|null
+{
+    return isset($_GET[$parametro]) ? trim($_GET[$parametro]) : null;
+}
+
+
 
 function volver_index (){
     header('Location: index.php');
@@ -17,20 +23,14 @@ function volver_index (){
 
 
 
-function validar_dni($dni ,&$error, ?PDO $pdo = null) {
+function validar_dni($dni ,&$error, ?PDO $pdo = null) 
+{
     if ($dni === '') {
             $error[] = 'El DNI es obligatorio';
         } else if ( mb_strlen($dni) > 9) {
                 $error[] = 'El DNI es demasiado largo';
         } else {  
-            //operador funcion de null devuelve el 1($pdo) si no es nulo ,
-            // si es nulo devuelve el 2(conectar()) actua en cortocircuito
-            $pdo = $pdo ?? conectar();
-            $sent = $pdo->prepare('SELECT * FROM CLIENTES WHERE dni = :dni');
-            $sent ->execute([':dni'=> $dni]);
-            
-            //Comprueba si el cliente  existe un cliente con le ese dni 
-            if ($sent->fetch()){
+            if (buscar_cliente_por_dni($dni,$pdo)){
                 $error[] = 'Ya existe un cliente con ese dni';
             }
         } 
@@ -96,4 +96,36 @@ function mostrar_errores(array $error): void
 }
 
 
+function buscar_cliente ($id,$pdo = null ) : array | false
+{
+    $pdo = $pdo ?? conectar();
+    $sent = $pdo->prepare ('SELECT * FROM clientes WHERE id = :id');
+    $sent->execute([':id'=> $id]);
+    return $sent->fetch();
+}
 
+function buscar_cliente_por_dni ($dni, ?PDO $pdo = null ) : array | false
+{
+    $pdo = $pdo ?? conectar();
+    $sent = $pdo->prepare ('SELECT * FROM clientes WHERE dni = :dni');
+    $sent->execute([':dni'=> $dni]);
+    return $sent->fetch();
+}
+
+
+
+function validar_dni_update($dni ,$id, &$error, ?PDO $pdo = null) 
+{
+    if ($dni === '') {
+            $error[] = 'El DNI es obligatorio';
+        } else if ( mb_strlen($dni) > 9) {
+            $error[] = 'El DNI es demasiado largo';
+        } else {  
+            $pdo = conectar();
+            $sent = $pdo->prepare('SELECT * FROM clientes WHERE dni = :dni and id != :id');
+            $sent->execute([':dni' => $dni,':id' => $id]);
+            if ($sent->fetch()){
+                $error[] = 'Ya existe un cliente con ese dni';
+            }
+        } 
+}
